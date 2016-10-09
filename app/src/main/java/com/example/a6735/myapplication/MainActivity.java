e1aea82f9bcff853a7165e2b5e04d5d9a746d8b7
Steven <a673576000@qq.com>
2016-10-09 19:18:08 +0800

    p
    p

--- app/src/main/java/com/example/a6735/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a6735/myapplication/MainActivity.java
@@ -15,9 +15,15 @@ import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URL;
 
 import pbmsg.UserProto;
 
@@ -38,14 +44,15 @@ public class MainActivity extends AppCompatActivity {
                 //网络请求必须在线程中完成
                 new  Thread() {
                     public void run() {
-                        send_text();
+                        //send_text();
+                        new_send();
                     }
                 }.start();
 
                 //接收
                 new Thread() {
                     public  void run() {
-                        get_text();
+                        //get_text();
                     }
                 }.start();
             }
@@ -90,29 +97,51 @@ public class MainActivity extends AppCompatActivity {
     }
     /*新的方式*/
     private void new_send() {
-        AndroidNetworking.initialize(getApplicationContext());
+        //AndroidNetworking.initialize(getApplicationContext());
 
         UserProto.User uproto = UserProto.User.newBuilder().setID(147258369)
                 .setPassword("123456").setUserName("jerome").build();
         byte[] content = uproto.toByteArray();
 
-        AndroidNetworking.post("192.168.1.103")
-                .addBodyParameter("firstname", "Amit")
-                .addBodyParameter("lastname", "Shekhar")
-                .addByteBody(content)
-                .setTag("test")
-                .setPriority(Priority.MEDIUM)
-                .build()
-                .getAsJSONArray(new JSONArrayRequestListener() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        // do anything with response
-                    }
-                    @Override
-                    public void onError(ANError error) {
-                        // handle error
-                    }
-                });
+//        AndroidNetworking.post("http://192.168.1.103:8080/HttpTest/")
+//                //.addBodyParameter("firstname", "Amit")
+//                //.addBodyParameter("lastname", "Shekhar")
+//                .addByteBody(content)
+//                .setTag("test")
+//                .setPriority(Priority.MEDIUM)
+//                .build()
+//                .getAsJSONArray(new JSONArrayRequestListener() {
+//                    @Override
+//                    public void onResponse(JSONArray response) {
+//                        // do anything with response
+//                    }
+//                    @Override
+//                    public void onError(ANError error) {
+//                        // handle error
+//                    }
+//                });
+        try {
+            URL httpUrl = new URL("http://192.168.1.103:8080/HttpTest");
+            HttpURLConnection conn = (HttpURLConnection)httpUrl.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setReadTimeout(5000);
+            OutputStream out = conn.getOutputStream();
+            String TTT="name=123&age=789";
+            out.write(TTT.getBytes());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuffer sb = new StringBuffer();
+            String str;
+            while ((str=reader.readLine())!=null) {
+                sb.append(str);
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
     }
 
 //    MobadsRequest adrequest = MobadsRequest.newBuilder().setRequestId(requestId).setAdslot(adslot).build();
